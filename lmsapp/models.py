7bddbc763e87db15fe6c17c8ef10e5ab16c80692
SaiSatwikMadddula <saisatwikmaddula@gmail.com>
2020-12-14 11:01:03 +0530

    Changed UI

--- lmsapp/models.py
+++ lmsapp/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
         ('education', 'Education'),
         ('entertainment', 'Entertainment'),
         ('comics', 'Comics'),
-        ('biography', 'Biographie'),
+        ('biography', 'Biography'),
         ('history', 'History'),
     ]
     name = models.CharField(max_length=30)
